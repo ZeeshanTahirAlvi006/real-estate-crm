13026036cd581639547db7b56eae1d84082a0928
Codewithgoostyhuman <zeeshantahiralvi123@gmail.com>
2026-09-09 11:46:03 +0500

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Automatically reload the page if a new deployment removed old chunk hashes
+window.addEventListener('vite:preloadError', (event) => {
+  event.preventDefault()
+  window.location.reload()
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
